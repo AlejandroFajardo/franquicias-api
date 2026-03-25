9101dfb4d7d31c42c3b35345c993fa315eb82ee9
Didier Alejandro Fajardo Annear <didierfajardo28@hotmail.com>
2026-03-25 03:43:20 -0500

    refactor: update domain models to support nested franquicia structure with sucursales and productos

--- src/main/java/com/challenge/accenture/franquicias_api/domain/model/Producto.java
+++ src/main/java/com/challenge/accenture/franquicias_api/domain/model/Producto.java
@@ -1,12 +1,14 @@
 package com.challenge.accenture.franquicias_api.domain.model;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Producto {
+    @Field("_id")
     private String id;
     private String nombre;
     private Integer stock;
--- src/main/java/com/challenge/accenture/franquicias_api/domain/model/Sucursal.java
+++ src/main/java/com/challenge/accenture/franquicias_api/domain/model/Sucursal.java
@@ -1,6 +1,8 @@
 package com.challenge.accenture.franquicias_api.domain.model;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Field;
+
 import java.util.List;
 
 @Data
@@ -8,6 +10,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class Sucursal {
+    @Field("_id")
     private String id;
     private String nombre;
     private List<Producto> productos;
